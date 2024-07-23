83b83649c98c955bfb04395ce7049612685da22d
Josh <josh2507@hotmail.co.uk>
2024-07-23 12:21:18 +0100

    added lines 8 and 9 for post

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import express from "express";
-const app = express();
-const port = 3000;
-app.get("/", (request, result) => {
+import express from "express"; //import from the node express modules
+const app = express(); //assigning express to a variable
+const port = 3000; //port is the local host e.g. localhost://3000 (PortNumber)
+app.get("/", (request, result) => { //use app instead of express as variable is assigned with the method .get
   result.send("test is ok");
 });
 
+app.post("/", (request, result) => {
+  result.send("Post has been requested")
+})
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`); //runs the server through your browser
 });
