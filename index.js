490c2d1f9cf50a75e18c925b4ad21f412389be23
Marco Di Summa <mdisumma@yahoo.com>
2024-07-23 13:15:56 +0100

    add comment

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express"; //import from the node express modules
 const app = express(); //assigning express to a variable
 const port = 3000; //port is the local host e.g. localhost://3000 (PortNumber)
-app.get("/", (request, result) => { //use app instead of express as variable is assigned with the method .get
+app.get("/", (request, result) => {
+  //use app instead of express as variable is assigned with the method .get
   result.send("test is ok");
 });
 
 app.post("/", (request, result) => {
-  result.send("Post has been requested")
-})
+  //use app instead of express as variable is assigned with the method .post
+  result.send("Post has been requested");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`); //runs the server through your browser
