ea65bb83b9366d377f5a67161e63adab0ddbad75
Marco Di Summa <mdisumma@yahoo.com>
2024-07-24 21:25:40 +0100

    implementing express-validator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import activities from "./activities.json" assert { type: "json" };
 import helmet from "helmet";
+import { query, validationResult } from "express-validator";
 import { v4 as uuidv4 } from "uuid";
 const app = express();
 const port = 3000;
@@ -56,46 +57,37 @@ app.post("/activities", (request, response) => {
   });
 });
 
+app.get("/hello", query("person").notEmpty().escape(), (req, res) => {
+  const result = validationResult(req);
+
+  res.send({ errors: result.array() });
+});
 //UPDATE(put) ACTIVITY
-app.put("/activities/:id/", (request, response) => {
-  // original activities
-  console.log("activities:", activities);
-  //request parameter
-  console.log("request params", request.params);
-  console.log("request body", request.body);
-  // parse parameter in json format and defragmentation.
-  const { activity_type, activity_duration } = request.body;
+//http://localhost:3000/activities?id=20
+app.put("/activities", query("id").notEmpty().escape(), (request, response) => {
+  const result = validationResult(request);
 
-  console.log("data:", activity_type, activity_duration);
+  if (result.isEmpty()) {
+    const index = activities.findIndex(
+      (activity) => activity.id === request.params["id"]
+    );
 
-  //query the request index by id
-  const index = activities.findIndex(
-    (activity) => activity.id === request.params["id"]
-  );
-  console.log("index:", index);
-
-  if (index === -1) {
-    response.status(400).json({
-      success: false,
-      payload: "please provide a valid id",
+    const { activity_type, activity_duration } = request.body;
+    const newActivity = {
+      id: uuidv4(),
+      activity_submitted: Date.now(),
+      activity_type,
+      activity_duration,
+    };
+    activities[index] = newActivity;
+    return response.status(200).json({
+      success: true,
+      query: request.query.id,
+      payload: activities,
     });
   }
 
-  //provide the new activity
-  const newActivity = {
-    id: uuidv4(),
-    activity_submitted: Date.now(),
-    activity_type,
-    activity_duration,
-  };
-  console.log("newActivities:", newActivity);
-  //update the activity
-  activities[index] = newActivity;
-  // response.send(activities);
-  response.status(200).json({
-    success: true,
-    payload: activities,
-  });
+  response.status(400).json({ errors: result.array() });
 });
 
 //DELETE ACTIVITY
