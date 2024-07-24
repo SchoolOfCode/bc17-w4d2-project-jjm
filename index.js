6202ebcda438ef92f48d104d8427123ea435eb95
Marco Di Summa <mdisumma@yahoo.com>
2024-07-24 12:27:58 +0100

    implementing nodemon

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { json } from "express"; //import from the node express modules
+import express, { json } from "express"; //import from the node express module
 import activities from "./activities.json" assert { type: "json" }; //import a json file from the activities.json
 const app = express(); //assigning express to a variable
 const port = 3000; //port is the local host e.g. localhost://3000 (PortNumber)
