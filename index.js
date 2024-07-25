d3d48536ae28f6fc75b4a8f25309e3faa1593909
Marco Di Summa <mdisumma@yahoo.com>
2024-07-25 11:08:22 +0100

    fixing bug

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import activities from "./activities.json" assert { type: "json" }; //import a j
 import helmet from "helmet"; //importing helmet
 const app = express(); //assigning express to a variable
 const port = 3000; //port is the local host e.g. localhost://3000 (PortNumber)
-console.log(activities)
+console.log(activities);
 app.use(express.json());
 app.use(helmet());
 //app.disable("x-powered-by"); //disabling x-powered-by feature
 
 app.use(
   helmet({
-    xPoweredBy: false,  //ignoring x-powered-by header
+    xPoweredBy: false, //ignoring x-powered-by header
   })
 );
 
@@ -18,23 +18,25 @@ console.log(activities);
 
 app.get("/", (request, response) => {
   //use app instead of express as variable is assigned with the method .get
+  console.log("ok");
   response.status(200).json({
     success: true,
     payload: "hello World",
-  })
+  });
 });
 
 app.get("/activities", (request, response) => {
   response.status(200).json({
     success: true,
     payload: activities,
-  })
-console.log(request)  //should show activity objects when accessed (ID, Date, Time etc) 
+  });
+  response.send(activities);
+  // console.log(request)  //should show activity objects when accessed (ID, Date, Time etc)
 
-//If there is an issue display error code representing the failure and why it happened
+  // //If there is an issue display error code representing the failure and why it happened
 
-//If someone sends something, send the correct status code, if nothing was sent, return an error stating the code and what the issue is
-})
+  // //If someone sends something, send the correct status code, if nothing was sent, return an error stating the code and what the issue is
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`); //runs the server through your browser
@@ -45,7 +47,6 @@ app.listen(port, () => {
 //response object always containing a key called data or error, ensure code is accurate represenation of failure or success and send back the response to the user
 
 //if the request succeeds, it will respond with the correct status code along with some User Acitivity Objects (Date, time, duration etc)
-
 
 // {
 //   data: [
